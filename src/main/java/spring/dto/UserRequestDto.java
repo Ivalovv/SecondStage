96e3dcc3aa1651ed6b8ac80be76d5029eeec6ad7
Ivan Pavlov <mega.iwan2033@gmail.com>
2026-02-02 17:17:13 +0300

    дз6

--- src/main/java/spring/dto/UserRequestDto.java
+++ src/main/java/spring/dto/UserRequestDto.java
@@ -1,19 +1,33 @@
 package spring.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
+@Schema(description = "DTO для создания / обновления пользователя")
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserRequestDto {
 
+    @Schema(
+            description = "Имя пользователя"
+    )
     @NotBlank
     private String name;
+
+    @Schema(
+            description = "Email пользователя"
+    )
     @NotBlank
     @Email
     private String email;
 
+    @Schema(
+            description = "Возраст пользователя",
+            minimum = "0",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private int age;
 }
--- src/main/java/spring/dto/UserResponseDto.java
+++ src/main/java/spring/dto/UserResponseDto.java
@@ -1,17 +1,38 @@
 package spring.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "DTO пользователя, возвращаемый API")
 @Getter
 @AllArgsConstructor
 public class UserResponseDto extends RepresentationModel<UserResponseDto> {
 
+    @Schema(
+            description = "Уникальный идентификатор пользователя"
+    )
     private Long id;
+
+    @Schema(
+            description = "Имя пользователя"
+    )
     private String name;
+
+    @Schema(
+            description = "Email пользователя"
+    )
     private String email;
+
+    @Schema(
+            description = "Возраст пользователя"
+    )
     private int age;
+
+    @Schema(
+            description = "Дата регистрации пользователя"
+    )
     private LocalDateTime createdAt;
 }
